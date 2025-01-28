2317c9bed33d1af812b001de8b53bb0c02a20522
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2025-01-28 20:48:34 -0300

    chore: Add ScoreDisplay

--- app/components/QuestionScreen.tsx
+++ app/components/QuestionScreen.tsx
@@ -1,3 +1,4 @@
+import { CircleCheck, CircleX } from "lucide-react";
 import Button from "./ui/Button";
 
 function QuestionScreen() {
@@ -16,6 +17,15 @@ function QuestionScreen() {
       </div>
 
       <Button variant="primary">Next Question</Button>
+
+      <div className="text-sm text-center">
+        <div className="flex items-center justify-center gap-2">
+          <CircleCheck className="text-green-500" />
+          <span className="text-green-500">1 correct</span>
+          <CircleX className="text-red-500" />
+          <span className="text-red-500">1 wrong</span>
+        </div>
+      </div>
     </>
   );
 }
