0e72ba85ea8dff5dff547ac2faed7d0579809ab6
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2025-01-28 20:09:51 -0300

    chore: add cn utils

--- app/components/ui/Select.tsx
+++ app/components/ui/Select.tsx
@@ -1,17 +1,24 @@
+import { cn } from "@/app/utils/cn";
 import { SelectHTMLAttributes } from "react";
 
 type Props = SelectHTMLAttributes<HTMLSelectElement> & {
   label: string;
 };
 
-function Select({ label, children }: Props) {
+function Select({ label, className, children, ...props }: Props) {
   return (
     <div>
       <label className="block text-sm font-medium mb-2 text-left">
         {label}
       </label>
 
-      <select className="w-full bg-gray-900 rounded-lg p-3 border-r-[12px] border-transparent">
+      <select
+        className={cn(
+          "w-full bg-gray-900 rounded-lg p-3 border-r-[12px] border-transparent",
+          className
+        )}
+        {...props}
+      >
         {children}
       </select>
     </div>
